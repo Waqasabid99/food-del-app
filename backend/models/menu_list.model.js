20f5121674a2716a31c429490eac8c4b2c75a9be
Waqasabid99 <waqasaliabid998@gmail.com>
2025-08-31 01:43:36 +0500

    removed backend folder (older version)

--- backend/models/menu_list.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const MenuSchema = new mongoose.Schema({
-  menu_name: { type: String, required: true },
-  menu_image: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Menu", MenuSchema);
--- backend/models/shop.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const ShopSchema = new mongoose.Schema({
-  food_list: [{ type: mongoose.Schema.Types.ObjectId, ref: "Food" }],
-  menu_list: [{ type: mongoose.Schema.Types.ObjectId, ref: "Menu" }],
-});
-
-module.exports = mongoose.model("Shop", ShopSchema);
